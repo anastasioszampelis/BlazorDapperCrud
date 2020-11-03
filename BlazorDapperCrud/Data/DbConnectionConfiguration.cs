7b0802b7733d21ea07327cbb3bb258bd84ed8152
Anastasios Zampelis <anastasioszampelis@gmail.com>
2020-11-04 00:09:04 +0200

    Initial

---
+++ BlazorDapperCrud/Data/DbConnectionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDapperCrud.Data
+{
+    public class DbConnectionConfiguration
+    {
+        public string Value { get; }
+        public DbConnectionConfiguration(string value) => Value = value;
+    }
+}
